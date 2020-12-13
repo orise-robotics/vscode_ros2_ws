36335613eae8e6f71409ef5e26d8d2f0cc071dc3
Mateus Amarante Araújo <mateus.amarujo@gmail.com>
2020-12-13 17:29:50 -0300

    Add script to setup vscode workspace against a running orise container

---
+++ setup_workspace.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+usage() {
+  printf "Usage: $0 CONTAINER_NAME\n"
+  printf "Setup vscode workspace for specified container\n\n"
+  printf "Options:\n"
+  printf "  -h|--help\t\t Shows this help message\n"
+  # printf "  -c|--container\t\t Container name\n"
+  # printf "  -d|--distro\t\t ROS distro (assumes container name is 'orise-$ROS_DISTRO-devel'\n"
+
+  exit 0
+}
+
+while [ -n "$1" ]; do
+  case $1 in
+  -h | --help) usage ;;
+  -?*)
+    echo "Unknown option '$1'" 1>&2
+    exit 1
+    ;;
+  *)
+    CONTAINER_NAME=$1
+    break
+    ;;
+  esac
+  shift
+done
+
+WORKING_DIR=$(docker inspect -f {{.Config.WorkingDir}} orise-noetic-devel)
+ROS_DISTRO=$(docker exec orise-noetic-devel /bin/bash -c "printenv ROS_DISTRO")
+COLCON_WORKSPACE_FOLDER=$(docker exec orise-noetic-devel /bin/bash -c "printenv COLCON_WORKSPACE_FOLDER")
+
+COLCON_WORKSPACE_FOLDER=${COLCON_WORKSPACE_FOLDER:-$WORKING_DIR}
+
+CONTAINER_CONFIG_FOLDER="$HOME/.config/Code/User/globalStorage/ms-vscode-remote.remote-containers/nameConfigs"
+
+if [ ! -d "${CONTAINER_CONFIG_FOLDER}" ]; then
+    mkdir -p "${CONTAINER_CONFIG_FOLDER}"
+fi
+
+echo '{"userEnvProbe": "loginInteractiveShell", "remoteUser": "orise"}' > "$CONTAINER_CONFIG_FOLDER/$CONTAINER_NAME.json"
+
+docker cp ros2.code-workspace $CONTAINER_NAME:$COLCON_WORKSPACE_FOLDER/
+docker cp .vscode-format/ $CONTAINER_NAME:$COLCON_WORKSPACE_FOLDER/
